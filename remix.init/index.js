3f8b7445271c50db8c2585da7aa7df73cd0deb86
Lucien George <luciengeorge95@gmail.com>
2024-04-01 11:03:12 +0300

    Add debugging console logs

--- remix.init/index.js
+++ remix.init/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const toml = require('@iarna/toml')
 const sort = require('sort-package-json')
-const {default: inquirer} = require('inquirer')
+const inquirer = require('inquirer')
 
 function escapeRegExp(string) {
   // $& means the whole matched string
@@ -24,16 +24,16 @@ async function main({rootDirectory}) {
   const PACKAGE_JSON_PATH = path.join(rootDirectory, 'package.json')
   const SITEMAP_PATH = path.join(rootDirectory, 'remix-sitemap.config.cjs')
   const SERVER_PATH = path.join(rootDirectory, 'server.js')
-
+  console.log('here1')
   const REPLACER = 'remix-express-template'
 
   const DIR_NAME = path.basename(rootDirectory)
   const SUFFIX = getRandomString(2)
-
+  console.log('here2')
   const APP_NAME = (DIR_NAME + '-' + SUFFIX)
     // get rid of anything that's not allowed in an app name
     .replace(/[^a-zA-Z0-9-_]/g, '-')
-
+  console.log('here3')
   const [prodContent, readme, env, packageJson, siteMap, serverFile] =
     await Promise.all([
       fs.readFile(FLY_TOML_PATH, 'utf-8'),
@@ -43,7 +43,7 @@ async function main({rootDirectory}) {
       fs.readFile(SITEMAP_PATH, 'utf-8'),
       fs.readFile(SERVER_PATH, 'utf-8'),
     ])
-
+  console.log('here4')
   env.replace(/^TOAST_SECRET=.*$/m, `TOAST_SECRET="${getRandomString(32)}"`)
   env.replace(/^CSRF_SECRET=.*$/m, `CSRF_SECRET="${getRandomString(32)}"`)
   env.replace(/^VERIFY_SECRET=.*$/m, `VERIFY_SECRET="${getRandomString(32)}"`)
@@ -55,22 +55,22 @@ async function main({rootDirectory}) {
     /^SESSION_SECRET=.*$/m,
     `SESSION_SECRET="${getRandomString(32)}"`,
   )
-
+  console.log('here5')
   const prodToml = toml.parse(prodContent)
   prodToml.app = prodToml.app.replace(REPLACER, APP_NAME)
-
+  console.log('here6')
   const newReadme = readme.replace(
     new RegExp(escapeRegExp(REPLACER), 'g'),
     APP_NAME,
   )
-
+  console.log('here7')
   const newPackageJson =
     JSON.stringify(
       sort({...JSON.parse(packageJson), name: APP_NAME}),
       null,
       2,
     ) + '\n'
-
+  console.log('here8')
   const {websiteUrl} = await inquirer.prompt([
     {
       type: 'input',
@@ -79,9 +79,9 @@ async function main({rootDirectory}) {
         'What is the URL of your website? It will be used for the sitemap. (Enter to skip)',
     },
   ])
-
+  console.log('here9')
   const trimmedWebsiteUrl = websiteUrl.trim()
-
+  console.log('here10')
   const newSitemap = siteMap.replace(
     /siteUrl: '<YOUR_DOMAIN_URL>'/,
     `siteUrl: '${trimmedWebsiteUrl}'`,
@@ -90,13 +90,13 @@ async function main({rootDirectory}) {
     /process.env.NODE_ENV === 'production'\n\s+\? '<YOUR_DOMAIN_URL>'/,
     `process.env.NODE_ENV === 'production'\n    ? '${trimmedWebsiteUrl}'`,
   )
-
+  console.log('here11')
   if (!trimmedWebsiteUrl) {
     console.log(
       `Skipped. Don't forget to update the sitemap with your website URL later in remix-sitemap.config.cjs and server.js.`,
     )
   }
-
+  console.log('here12')
   await Promise.all([
     fs.writeFile(FLY_TOML_PATH, toml.stringify(prodToml)),
     fs.writeFile(README_PATH, newReadme),
@@ -113,7 +113,7 @@ async function main({rootDirectory}) {
       ? fs.writeFile(SERVER_PATH, newServerFile)
       : Promise.resolve(),
   ])
-
+  console.log('here13')
   execSync(`npm run setup`, {stdio: 'inherit', cwd: rootDirectory})
 
   console.log(
